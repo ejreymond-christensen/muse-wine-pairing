b3ec9d7222ab6b1fb1b109e61a0057e7b4a53eb6
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-02-06 16:20:28 -0600

    moved jQuery population function out of ajax call back and into addRecipePreview, so that the api is not called on new recipe request with same search details

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,38 +3,37 @@ $(document).ready(function() {
   $('select').material_select();
 });
 
+var recipeData=[];
+
 function getRecipe(queryURL) {
 
 	$.ajax({
 		"url": queryURL,
 		"method": "GET"
-	}).then(function(recipeData) {
-		console.log(recipeData);
-		console.log(recipeData.matches[0].imageUrlsBySize[90]);
-		var imageURL = (recipeData.matches[0].smallImageUrls[0]).slice(0,-2)+"500-c";
-		console.log(imageURL);
-
-    var mealCard= $('<div class="col s7">');
-    var cardContents = $('<div class="card ">');
-    var imageSection = $('<div class="card-image">');
-    imageSection.append('<img class= "recipeImg" src="'+imageURL+'">');
-    imageSection.append('<span class="card-title">'+recipeData.matches[0].recipeName+'</span>');
-    imageSection.append('<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>');
-    var textSection = $('<div class="card-content">');
-    textSection.append('<p>here is some info we bring in from the api</p>');
-
-    textSection.append('<span class= "yummlyAttr">'+recipeData.attribution.html+'</span>');
-
-
-    cardContents.append(imageSection);
-    cardContents.append(textSection);
-    mealCard.append(cardContents);
-    $('#cardHolder').empty();
-    $('#cardHolder').append(mealCard);
-
-
-
+	}).then(function(recipeDataArray) {
+    recipeData=recipeDataArray;
+    addRecipePreview(recipeData);
 	});
+}
+function addRecipePreview(recipeData){
+  var randomNumber= Math.floor(Math.random() * 50);
+
+  var imageURL = (recipeData.matches[randomNumber].smallImageUrls[0]).slice(0,-2)+"500-c";
+
+  var mealCard= $('<div class="col s7">');
+  var cardContents = $('<div class="card ">');
+  var imageSection = $('<div class="card-image">');
+  imageSection.append('<img class= "recipeImg" src="'+imageURL+'">');
+  imageSection.append('<span class="card-title">'+recipeData.matches[randomNumber].recipeName+'</span>');
+  imageSection.append('<a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>');
+  var textSection = $('<div class="card-content">');
+  textSection.append('<p>here is some info we bring in from the api</p>');
+  textSection.append('<span class= "yummlyAttr">'+recipeData.attribution.html+'</span>');
+  cardContents.append(imageSection);
+  cardContents.append(textSection);
+  mealCard.append(cardContents);
+  $('#cardHolder').empty();
+  $('#cardHolder').append(mealCard);
 }
 
 $("#searchFood").on("click", function(){
@@ -42,9 +41,17 @@ $("#searchFood").on("click", function(){
   var cuisine= $("#cuisine").val();
   var ingredients= $("#ingredients").val().join("+");
   console.log("ingredients"+ $("#ingredients").val());
-  var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=78251404&_app_key=ae65a091779f9ce10b68bb9d74e5ebdf&q="+meat+"+"+ingredients+"&requirePictures=true&allowedCuisine[]=cuisine^cuisine-"+cuisine;
+  var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=78251404&_app_key=ae65a091779f9ce10b68bb9d74e5ebdf&q="+meat+"+"+ingredients+"&maxResult=50&requirePictures=true";
+
+  for (var i = 0; i < cuisine.length; i ++){
+   var cuisineAdd = "&allowedCuisine[]=cuisine^cuisine-" + cuisine[i];
+   queryURL= queryURL+cuisineAdd;
+   console.log(queryURL);
+
+ }
+
   getRecipe(queryURL);
-  console.log(ingredients);
+  console.log(queryURL);
 });
 
 
